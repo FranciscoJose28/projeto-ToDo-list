22753469fc67cd8a528022d965d77ededb3daa84
FranciscoJose28 <fcojmatos28@gmail.com>
2024-05-09 12:04:29 -0300

    Inicio de projeto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import "primereact/resources/themes/lara-dark-green/theme.css";
+import "primeflex/primeflex.css";
+import "primeicons/primeicons.css";
+
+const App = () => {
+  return (
+    <>
+    
+    </>
+  );
+}
+ 
+export default App;
