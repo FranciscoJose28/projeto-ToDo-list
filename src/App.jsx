ce6b6fa2ee6d6948788f788bfe698a6b5507e7ca
FranciscoJose28 <fcojmatos28@gmail.com>
2024-05-14 12:14:55 -0300

    continuar proxima aula

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 
-
-import "primereact/resources/themes/lara-dark-green/theme.css";
+import "primereact/resources/themes/lara-light-amber/theme.css";
 import "primeflex/primeflex.css";
 import "primeicons/primeicons.css";
+import Paths from "./routes/Paths";
 
 const App = () => {
   return (
     <>
-    
+      <Paths />
     </>
   );
 }
